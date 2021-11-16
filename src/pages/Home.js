dd76510266aa12bc909d9a2a422a8e78532967b7
Ethan Roberts <Ethan.Roberts@dyson.com>
2021-11-16 15:58:18 +0000

    centre align home content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Blogs from "../components/Blogs"
 import ThingsIveMade from "../components/ThingsIveMade"
+import Container from "react-bootstrap/cjs/Container";
 
 function Home() {
   return (
-      <>
-        <ThingsIveMade/>
-        <Blogs/>
-      </>
+      <Container className="h-100 d-flex align-items-center">
+        <div>
+          <ThingsIveMade/>
+          <Blogs/>
+        </div>
+      </Container>
   )
 }
 
