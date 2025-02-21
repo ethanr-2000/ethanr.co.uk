dc5daa05a0864f9755715705087bc8b4fbef2c4e
Ethan Roberts <robee031@gmail.com>
2025-02-21 19:34:33 +0000

    chore: dont cache bundle

--- packages/terraform/components/ethanr/locals.tf
+++ packages/terraform/components/ethanr/locals.tf
@@ -2,5 +2,5 @@ locals {
   resolved_hosted_zone_id = var.hosted_zone_id != "" ? var.hosted_zone_id : aws_route53_zone.main[0].zone_id
 
   # get package version from package.json
-  package_version = jsondecode(file("${path.module}/../../../../package.json")).version
+  package_version = jsondecode(file("${path.module}/../../package.json")).version
 }
