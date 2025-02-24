57bdb8435729f15e00f919d3269714d64bce70d6
Ethan Roberts <robee031@gmail.com>
2025-02-24 22:07:34 +0000

    fix: lambda logging iam

--- packages/terraform/modules/project/bad-spelling-generator/aws_iam_policy.lambda_logging.tf
+++ packages/terraform/modules/project/bad-spelling-generator/aws_iam_policy.lambda_logging.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
+  name        = "${local.unique_id}-logging"
   path        = "/"
   description = "IAM policy for logging from a lambda"
   policy      = data.aws_iam_policy_document.lambda_logging.json
